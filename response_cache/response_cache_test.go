6e4be6e230bb84fcd79f0d6da18b465f38c33867
Will Bryant <will.bryant@gmail.com>
2016-11-29 00:04:06 +1300

    add test coverage for ResponseCache

---
+++ response_cache/response_cache_test.go
@@ -0,0 +1,36 @@
+package response_cache
+
+import "testing"
+import "reflect"
+
+func dummyCacheEntry() Entry {
+	entry := NewCacheEntry()
+	entry.Status = 200
+	entry.Header.Add("Host", "www.example.com")
+	entry.Header.Add("Content-Type", "text/html")
+	entry.Body = []byte("Test response body\x00test.")
+	return entry
+}
+
+func TestResponseCacheSetAndGet(t *testing.T) {
+	cache := NewResponseCache("test")
+
+	entry := dummyCacheEntry()
+	cache.Set("key1", entry)
+	retrieved, ok := cache.Get("key1")
+
+	if !ok { t.Error("Cache did not contain written key") }
+	if entry.Status != retrieved.Status { t.Error("Status was not restored from the cache") }
+	if !reflect.DeepEqual(entry.Header, retrieved.Header) { t.Error("Header was not restored from the cache") }
+	if !reflect.DeepEqual(entry.Body, retrieved.Body) { t.Error("Body was not restored from the cache") }
+}
+
+func TestResponseCacheGetAbsentKey(t *testing.T) {
+	cache := NewResponseCache("test")
+
+	entry := dummyCacheEntry()
+	cache.Set("key1", entry)
+	_, ok := cache.Get("key2")
+
+	if ok { t.Error("Cache should not contain key not set") }
+}
